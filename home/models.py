8d438f24a22153406a37e07680a86e123361aa1c
amirmovafagh <amir.movafagh@gmail.com>
2021-09-11 23:07:28 +0430

    add template files

--- home/models.py
+++ home/models.py
@@ -1,3 +1,58 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+from extensions.utils import jalali_converter
+
+
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'فعال'),
+        ('False', 'غیرغعال'),
+    )
+    title = models.CharField(max_length=150, verbose_name='عنوان وب سایت')
+    keywords = models.CharField(max_length=255, verbose_name='کلمات کلیدی')
+    description = models.CharField(max_length=255, verbose_name='عنوان')
+    company = models.CharField(blank=True, max_length=50, verbose_name='شرکت')
+    address = models.CharField(blank=True, max_length=255, verbose_name='آدرس')
+    phone = models.CharField(blank=True, max_length=20,
+                             verbose_name="تلفن همراه (جهت دریافت اطلاع رسانی پیامکی وبسایت)")
+    phone2 = models.CharField(blank=True, max_length=20, verbose_name='شماره تماس ثابت')
+    fax = models.CharField(blank=True, max_length=20, verbose_name='فکس')
+    email = models.CharField(blank=True, max_length=70, verbose_name='ایمیل')
+
+    icon = models.ImageField(upload_to='images/', verbose_name='لوگو'
+                             , help_text='لوگو اصلی نمایش در بالای سایت')
+    shortcut_icon = models.ImageField(blank=True, upload_to='images/', verbose_name='لوگو کوچک',
+                                      help_text='نسبت 1.1 به منظور نمایش در تب مرورگر')
+    facecbook = models.CharField(blank=True, max_length=100, verbose_name='فیسبوک')
+    instagram = models.CharField(blank=True, max_length=100, verbose_name='اینستاگرام')
+    telegram = models.CharField(blank=True, max_length=100, verbose_name='تلگرام')
+    youtube = models.CharField(blank=True, max_length=100, verbose_name='یوتیوب')
+    twitter = models.CharField(blank=True, max_length=100, verbose_name='توییتر')
+    linkedin = models.CharField(blank=True, max_length=100)
+    aboutus = RichTextUploadingField(blank=True, verbose_name='درباره ما')
+    contact = RichTextUploadingField(blank=True, verbose_name='تماس با ما')
+    worktime = RichTextUploadingField(blank=True, verbose_name='ساعت کاری')
+    customerservices = RichTextUploadingField(blank=True, verbose_name='خدمات مشتریان')
+    notices = RichTextUploadingField(blank=True, verbose_name="اعلامیه وبسایت")
+    status = models.CharField(max_length=20, choices=STATUS, verbose_name='وضعیت')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ بروزرسانی')
+
+    def __String__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'اطلاعات وب سایت'
+        verbose_name_plural = 'اطلاعات سایت'
+
+    def j_date(self):
+        return jalali_converter(self.create_at)
+
+    j_date.short_description = 'تاریخ'
+
+    def get_absolute_url(self):
+        return reverse("home:AboutMe")
---
+++ home/templatetags/custom_tags.py
@@ -0,0 +1,95 @@
+from datetime import timedelta, datetime
+
+from django import template
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Count, Q
+
+from home.models import Setting
+
+register = template.Library()
+
+
+# @register.filter(name='persian_int')
+# def persian_int(english_int):
+#     devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
+#     number = str(english_int)
+#     return ''.join(devanagari_nums[int(digit)] for digit in number)
+#
+#
+# @register.simple_tag
+# def categorylist():
+#     return Category.objects.filter(status=True)
+
+
+# @register.inclusion_tag("category_navbar.html")
+# def category_navbar():
+#     return {'category': Category.objects.filter(status='True', )}
+#
+#
+# @register.inclusion_tag("article_category_navbar.html")
+# def category_navbar_article():
+#     return {
+#         "category": ArticleCategory.objects.filter(status=True)
+#     }
+
+
+@register.simple_tag
+def website_info():
+    if Setting.objects.exists():
+        return Setting.objects.get(pk=1)
+    return None
+
+
+# @register.simple_tag
+# def shopcart_count(userid):
+#     return ShopCart.objects.count(user_id=userid)
+
+#
+# @register.simple_tag
+# def shopcart_info(userid):
+#     return ShopCart.objects.filter(user_id=userid)
+#
+#
+# @register.inclusion_tag("adminlte/link.html")
+# def link(request, link_name, content):
+#     return {
+#         "request": request,
+#         "link_name": link_name,
+#         "link": "user:{}".format(link_name),
+#         "content": content,
+#     }
+#
+#
+# @register.inclusion_tag("link.html")
+# def linkp(request, icon, link_name, content):
+#     return {
+#         "request": request,
+#         "link_name": link_name,
+#         "icon": icon,
+#         "link": "{}".format(link_name),
+#         "content": content,
+#     }
+#
+#
+# @register.inclusion_tag("home/products_suggest.html")
+# def popular_products():
+#     last_month = datetime.today() - timedelta(days=30)
+#     return {
+#         "products": Product.objects.active().annotate(
+#             count=Count('hits', filter=Q(producthit__create_at__gt=last_month))
+#         ).order_by('-count', '-create_at')[:16],
+#         "title": "پربازدیدترین‌ها",
+#     }
+#
+#
+# @register.inclusion_tag("home/products_suggest.html")
+# def hot_products():
+#     last_month = datetime.today() - timedelta(days=30)
+#     content_type_id = ContentType.objects.get(app_label='product', model='product').id
+#     return {
+#         "products": Product.objects.active().annotate(
+#             count=Count('hits',
+#                         filter=Q(comments__posted__gt=last_month) and Q(comments__content_type_id=content_type_id))
+#         ).order_by('-count', '-create_at')[:16],
+#         "title": "محصولات داغ",
+#     }
